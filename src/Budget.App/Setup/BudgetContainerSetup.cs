a0321af6a372332ae24754736bc58602404bb33e
Miguel Veloso <mvelosop@gmail.com>
2017-11-28 11:25:12 +0000

    Initial application setup

---
+++ src/Budget.App/Setup/BudgetContainerSetup.cs
@@ -0,0 +1,32 @@
+﻿using Autofac;
+
+namespace Budget.Setup
+{
+    public class BudgetContainerSetup : Module
+    {
+        private readonly BudgetDbSetup _dbSetup;
+
+        public BudgetContainerSetup(BudgetDbSetup dbSetup)
+        {
+            _dbSetup = dbSetup;
+        }
+
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.Register(c => _dbSetup.CreateDbContext())
+                .InstancePerLifetimeScope();
+
+            builder.RegisterAssemblyTypes(ThisAssembly)
+                .Where(t => t.Name.EndsWith("Repository"))
+                .AsImplementedInterfaces()
+                .AsSelf()
+                .InstancePerLifetimeScope();
+
+            builder.RegisterAssemblyTypes(ThisAssembly)
+                .Where(t => t.Name.EndsWith("Services"))
+                .AsImplementedInterfaces()
+                .AsSelf()
+                .InstancePerLifetimeScope();
+        }
+    }
+}
---
+++ src/Budget.App/Setup/BudgetDbSetup.cs
@@ -0,0 +1,48 @@
+﻿using Budget.Data.Services;
+using Microsoft.EntityFrameworkCore;
+using System;
+using Microsoft.EntityFrameworkCore.Query.ExpressionTranslators.Internal;
+
+namespace Budget.Setup
+{
+    public class BudgetDbSetup
+    {
+        private readonly string _connectionString;
+
+        private readonly object _lock = new object();
+
+        private DbContextOptions<BudgetDbContext> _options;
+
+        public BudgetDbSetup(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public void ConfigureDatabase(bool migrateDatabase = false)
+        {
+            var optionBuilder = new DbContextOptionsBuilder<BudgetDbContext>();
+
+            optionBuilder.UseSqlServer(_connectionString);
+
+            _options = optionBuilder.Options;
+
+            if (!migrateDatabase) return;
+
+            // This lock avoids conflicts on DB creation, specially during parallel integration tests
+            lock (_lock)
+            {
+                using (var dbContext = CreateDbContext())
+                {
+                    dbContext.Database.Migrate();
+                }
+            }
+        }
+
+        public BudgetDbContext CreateDbContext()
+        {
+            if (_options == null) throw new InvalidOperationException($"Must run {GetType().Name}.{nameof(ConfigureDatabase)} first!");
+
+            return new BudgetDbContext(_options);
+        }
+    }
+}
