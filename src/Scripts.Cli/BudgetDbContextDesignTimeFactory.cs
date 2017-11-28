0340fe418f859c601dda061509870f364eb2ffc4
Miguel Veloso <mvelosop@gmail.com>
2017-11-28 11:27:13 +0000

    Step 1 - Add initial migration

---
+++ src/Scripts.Cli/BudgetDbContextDesignTimeFactory.cs
@@ -0,0 +1,21 @@
+﻿using Budget.Data.Services;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace Scripts.Cli
+{
+    // 1-1. Add DbContext factory
+    //---------------------------
+
+    //public class BudgetDbContextDesignTimeFactory : IDesignTimeDbContextFactory<BudgetDbContext>
+    //{
+    //    public BudgetDbContext CreateDbContext(string[] args)
+    //    {
+    //        var builder = new DbContextOptionsBuilder<BudgetDbContext>();
+
+    //        builder.UseSqlServer("x");
+
+    //        return new BudgetDbContext(builder.Options);
+    //    }
+    //}
+}
--- src/Scripts.Cli/Program.cs
+++ src/Scripts.Cli/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using Budget.Data.Services;
+using Microsoft.EntityFrameworkCore;
 
 namespace Scripts.Cli
 {
@@ -7,6 +9,25 @@ namespace Scripts.Cli
         static void Main(string[] args)
         {
             Console.WriteLine("Host for EF scripts");
+
+            // 1-2. Initialize DbContext and apply migration to verify it's working
+            //---------------------------------------------------------------------
+
+            //string connectionString =
+            //    "Server=localhost; Initial Catalog=SpecFlowEFCore2.Cli; Trusted_Connection=true; MultipleActiveResultSets=true;";
+
+            //var optionsBuilder = new DbContextOptionsBuilder<BudgetDbContext>();
+
+            //optionsBuilder.UseSqlServer(connectionString);
+
+            //Console.WriteLine("Creating database / applying migrations...");
+
+            //using (var dbContext = new BudgetDbContext(optionsBuilder.Options))
+            //{
+            //    dbContext.Database.Migrate();
+            //}
+
+            //Console.WriteLine("Done!");
         }
     }
 }
